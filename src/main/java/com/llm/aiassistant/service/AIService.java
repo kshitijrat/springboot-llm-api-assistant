6edaf275b6b96248570a0a4782a3215bbe0845c3
kshitijrat <kshitijratnawat@gmail.com>
2026-03-14 10:18:25 +0530

    Update Service

--- src/main/java/com/llm/aiassistant/service/AIService.java
+++ src/main/java/com/llm/aiassistant/service/AIService.java
@@ -1,3 +1,13 @@
+package com.llm.aiassistant.service;
+
+import org.springframework.web.client.RestTemplate;
+
 public class AIService {
-    
+    private final String API_KEY = "YOUR_GEMINI_API_KEY";
+    public String getAPIResponse(String question){
+        try{
+            String url = "";
+            RestTemplate restTemplate = new RestTemplate();
+        }
+    }
 }
