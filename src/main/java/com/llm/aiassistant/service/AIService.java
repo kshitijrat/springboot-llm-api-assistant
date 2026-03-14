2807d5f4961c0df81a3fcd3d693d47de2e4a9318
kshitijrat <kshitijratnawat@gmail.com>
2026-03-14 10:24:32 +0530

    Solve error api key not found

--- src/main/java/com/llm/aiassistant/service/AIService.java
+++ src/main/java/com/llm/aiassistant/service/AIService.java
@@ -1,14 +1,50 @@
 package com.llm.aiassistant.service;
 
+import org.json.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 public class AIService {
     private final String API_KEY = "YOUR_GEMINI_API_KEY";
-    public String getAPIResponse(String question){
-        try{
-            String url = "";
+
+    public String getAPIResponse(String question) {
+        try {
+            String url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key="
+                    + API_KEY;
             RestTemplate restTemplate = new RestTemplate();
-            
+            JSONObject reqBody = new JSONObject();
+            reqBody.put("contents", new Object[] {
+                    new JSONObject().put("parts", new Object[] {
+                            new JSONObject().put("text", question)
+                    })
+            });
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            HttpEntity<String> entity = new HttpEntity<>(reqBody.toString(), headers);
+
+            ResponseEntity<String> response = restTemplate.exchange(
+                    url,
+                    HttpMethod.POST,
+                    entity,
+                    String.class);
+            JSONObject jsonResponse = new JSONObject(response.getBody());
+
+            return jsonResponse
+                    .getJSONArray("candidates")
+                    .getJSONObject(0)
+                    .getJSONObject("content")
+                    .getJSONArray("parts")
+                    .getJSONObject(0)
+                    .getString("text");
+
+        } catch (Exception e) {
+            return "Error calling AI service: " + e.getMessage();
         }
     }
 }
